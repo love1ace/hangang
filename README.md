108400af3bb8aa1e0625ba22cfea292d5b9861ef
love1ace <lovelacedud@gmail.com>
2024-10-15 10:52:01 +0900

    v1.0.0

---
+++ README.md
@@ -0,0 +1,18 @@
+# hangang
+
+hangang is a proud South Korean author who won the Nobel Prize in Literature.
+
+![npm](https://img.shields.io/npm/v/hangang)
+![license](https://img.shields.io/npm/l/hangang)
+
+![alt text](image.png)
+
+## Quick Start
+
+Get **hangang** up and running quickly!
+
+1. **Run**:
+    ```bash
+    npx hangang
+    ```
+2. **Done!**
---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const messages = {
+  korean: "한강 작가님 노벨 문학상 축하드립니다",
+  english: "Congratulations to Hangang on winning the Nobel Prize in Literature",
+  spanish: "Felicitaciones a Hangang por ganar el Premio Nobel de Literatura",
+  french: "Félicitations à Hangang pour avoir remporté le Prix Nobel de Littérature",
+  german: "Herzlichen Glückwunsch an Hangang zum Gewinn des Nobelpreises für Literatur",
+  japanese: "ハンガン作家、ノーベル文学賞おめでとうございます",
+  chinese: "恭喜韩江作家获得诺贝尔文学奖",
+  italian: "Congratulazioni a Hangang per aver vinto il Premio Nobel per la Letteratura"
+}
+console.log();
+console.log(messages.korean);
+console.log(messages.english);
+console.log(messages.spanish);
+console.log(messages.french);
+console.log(messages.german);
+console.log(messages.japanese);
+console.log(messages.chinese);
+console.log(messages.italian);
+console.log();
